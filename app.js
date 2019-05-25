12ed093a150f56dc291feec83e160c572338475b
kasule joseph <joseph.kasule@andela.com>
2019-05-25 18:39:49 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const validator = require('validator')
+const yargs = require('yargs')
+const chalk = require('chalk')
+const notesUtils = require('./notes')
+
+// create a note
+yargs.command({
+    command: 'add',
+    description: 'Adds a new note',
+    builder: {
+        title: {
+            demandOption: true,
+            type: 'string',
+            default: 'Default title'
+        },
+        body: {
+            type: 'string',
+            default: 'Node js is awesome'
+        }
+    },
+    handler: function (argv) {
+        notesUtils.addNotes(argv.title, argv.body)
+        
+    }
+})
+
+// Remove a note
+yargs.command({
+    command: 'remove',
+    description: 'Remove a note',
+    builder: {
+        title: {
+            demandOption: true,
+            type: 'string',
+        }
+    },
+    handler: function (argv) {
+        notesUtils.removeNotes(argv.title)
+    }
+})
+
+// List all notes 
+yargs.command({
+    command: 'get',
+    description: 'List all notes',
+    handler: function () {
+        console.log('List all notes...')
+    }
+})
+
+// read a note
+yargs.command({
+    command: 'read',
+    description: 'Read a note',
+    handler: function () {
+        console.log('Reading a note...')
+    }
+})
+
+// console.log(process.argv[process.argv.length-1])
+yargs.parse()
---
+++ utils.js
@@ -0,0 +1,7 @@
+console.log('kasule in utils')
+
+const sub = function (a, b, c) {
+    return c-b-a 
+}
+
+module.exports = sub
