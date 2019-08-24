4eac8cad4f1764685bed995f07bf4f108faacf5f
kasule joseph <joseph.kasule@andela.com>
2019-08-24 22:15:40 +0200

    ft(mongoose): add some validations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,27 +1,51 @@
 const mongoose = require('mongoose')
-
+const validator = require('validator')
 const connection = mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
 const User = mongoose.model('Users', {
-    name: {type: String},
-    age: {type: Number}
+    name: {
+        type: String,
+        trim: true
+    },
+    
+    email: {
+        type: String,
+        require: true,
+        trim: true,
+        required: true,
+        lowercase: true,
+        validate(value){
+            if(!validator.isEmail(value)) {
+                throw new Error('Invalid Email address')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be greater than zero')
+            }
+        }
+    }
 })
 
 const newUser = new User({
-    name: 'kasule',
-    age: 'dgjdrn'
+    name: 'ka   sule',
+    email: 'kasule08joseph@gmail.com',
+    age: 50
 })
 
-// newUser.save().then((result) => {
-//     console.log(result);
+newUser.save().then((result) => {
+    console.log(result);
     
-// }).catch((error) => {
-//     console.log(error);
+}).catch((error) => {
+    console.log(error);
     
-// })
+})
 
 const task = mongoose.model('Tasks', {
     desc: {
@@ -37,10 +61,10 @@ const newTask = new task({
     complete: true
 })
 
-newTask.save().then((result) => {
-    console.log(result);
+// newTask.save().then((result) => {
+//     console.log(result);
     
-}).catch((error) => {
-    console.log('error', error);
+// }).catch((error) => {
+//     console.log('error', error);
     
-})
+// })
