4f00f00fa073b4748576703a5d0ca129b5a7796d
kasule joseph <joseph.kasule@andela.com>
2019-08-26 20:05:33 +0200

    read singular user and all users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import './src/db/mongoose'
 import User from './models/user'
 import Task from './models/task'
+import { log } from 'util';
 
 const app = express()
 const port = process.env.PORT || 8000
@@ -23,6 +24,51 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    let userCount;
+    User.countDocuments({}, (error, count) => {
+        userCount = count
+        
+    });
+    
+    User.find({}).then((users) => {
+        res.status(200).send({
+            status: 200,
+            user_count: userCount,
+            data: users
+        })
+
+    }).catch((error) => {
+        res.status(500).send({
+            status: 500,
+            error: error
+        })
+
+    })
+})
+
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {        
+        if(!user) {
+            return res.status(404).send({
+                status: 404,
+                error: 'User Not Found'
+            })
+        }
+        res.status(200).send({
+            status: 200,
+            data: user
+        })
+    }).catch((error) => {
+        res.status(404).send({
+            status: 404,
+            error: error
+        })
+    })
+    
+})
 app.post('/task', (req, res) => {
     const task = new Task(req.body)
 
