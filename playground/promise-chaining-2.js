b667a17b9adcced3174e661131b504a1ed0f79ec
kasule joseph <joseph.kasule@andela.com>
2019-08-27 15:16:56 +0200

    async and await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,13 +2,29 @@ import '../src/db/mongoose'
 
 import Task from '../models/task'
 
-Task.findByIdAndDelete('5d625b572cb945dfffe9f131').then((task) => {
-    console.log(task);
-    return Task.countDocuments({complete: false})
-}).then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete('5d625b572cb945dfffe9f131').then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({complete: false})
+// }).then((result) => {
+//     console.log(result);
+    
+// }).catch((e) => {
+//     console.log(e);
+    
+// })
+
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    console.log('taske', task);
+    
+    const count = await Task.countDocuments({complete: false})
+    return count
+}
+
+deleteTaskAndCount('5d641454902d0049e3c5c240').then((count) => {
+    console.log('count', count);
     
 }).catch((e) => {
-    console.log(e);
+    console.log('e', e);
     
 })
