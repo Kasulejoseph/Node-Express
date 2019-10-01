9875dd811b9ef9e52918a857c6a2378b008b3d53
kasule joseph <joseph.kasule@andela.com>
2019-10-01 14:36:04 +0200

    mock external node modules

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,11 +1,20 @@
 import request from 'supertest'
+import jwt from 'jsonwebtoken'
+import mongoose from 'mongoose'
 import app from '../app'
 import User from '../models/user'
+import dotenv from 'dotenv'
+const userId = mongoose.Types.ObjectId()
 
+dotenv.config()
 const loggedUser = {
+    _id: userId,
     email: 'joseph@g.com',
     name: 'joseph',
-    password: '12qwert@!'
+    password: '12qwert@!',
+    tokens: [{
+        token: jwt.sign({_id: userId}, process.env.SECRETKEY)
+    }]
 }
 
 beforeEach(async() => {
@@ -14,18 +23,33 @@ beforeEach(async() => {
 
 })
 test('user should create a account', async () => {
-    await request(app).post('/users').send({
+    const response = await request(app).post('/users').send({
         name: 'kasule', 
         email: 'kasule@ex.com',
         password: 'Mypass!!!'
     }).expect(201)
+
+    // Assert database changes    
+    const user = await User.findById(response.body.data._id)
+    expect(user).not.toBeNull()    
+    // Assert object
+    expect(response.body).toMatchObject({
+        data: {
+            name: 'kasule',
+            email: 'kasule@ex.com',
+        },
+        token: user.tokens[0].token
+    })
+    expect(user.password).not.toBe('12qwert@!')
 })
 
 test('User should login sucessfully', async() => {
-    await request(app).post('/users/login').send({
+    const response = await request(app).post('/users/login').send({
         email: loggedUser.email,
         password: loggedUser.password
     }).expect(200)
+    const user = await User.findById(response.body.data._id)    
+    expect(response.body.token).toBe(user.tokens[1].token)
 })
 
 test('User should not login with invalid credentions', async() => {
@@ -33,4 +57,38 @@ test('User should not login with invalid credentions', async() => {
         email: loggedUser.email,
         password: 'password'
     }).expect(400)
+})
+
+test('should get user profile', async() => {
+    await request(app)
+        .get('/users/me')
+        .set({'Authorization': loggedUser.tokens[0].token})
+        .send()
+        .expect(200)
+})
+
+test('should not get profile for unathorized users', async() => {
+    await request(app)
+        .get('/users/me')
+        .send()
+        .expect(401)
+})
+
+test('should delete a user', async() => {
+    const response = await request(app)
+        .delete('/users/me')
+        .set({'Authorization': loggedUser.tokens[0].token})
+        .send({})
+        .expect(200)
+        const user = await User.findById(response.body.data._id)    
+        expect(user).toBeNull()
+})
+
+test('should not delete a user when not authenticated', async() => {
+    await request(app)
+        .delete('/users/me')
+        .send()
+        .expect(401)
+    const user = await User.findById(userId)    
+    expect(user).not.toBeNull()
 })
