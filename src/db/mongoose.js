32ca59bbafbbd78a6c8007a97c72d12e0807d80d
kasule joseph <joseph.kasule@andela.com>
2019-10-19 09:36:19 +0200

    fix timeout error

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,10 +5,10 @@ dotenv.config()
 
 
 
-let db_url = process.env.NODE_ENV === 'production'? process.env.LOCAL_URL : process.env.LOCAL_URL
+let db_url = process.env.NODE_ENV === 'production'? process.env.MONGODB_URL : process.env.LOCAL_URL
 
 if(process.env.NODE_ENV === 'testing'){
-    db_url = process.env.TEST_URL
+    db_url = 'mongodb://kasule:casjos06@cluster0-shard-00-00-zj21u.mongodb.net:27017,cluster0-shard-00-01-zj21u.mongodb.net:27017,cluster0-shard-00-02-zj21u.mongodb.net:27017/task-manager-api-test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority'
 }
 
 mongoose.connect(db_url, {
